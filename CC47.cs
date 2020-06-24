a5f6d781efea6e058799df1c69b65d3e42d0cb89
Tome Sebty <tomcb@walla.com>
2020-06-24 20:34:05 +0300

    Coding Challenge 47 for NextGenCoder

--- CC47.cs
+++ CC47.cs
@@ -1,7 +1,25 @@
+using System;
+
 namespace CodingChallenges
 {
     public class CC47
-    {
-        
+    {       
+        public static string Encrypt(string word)
+        {
+            char c;
+            string newWord = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] >= 'a' && word[i] <= 'z')
+                    c = (char)(219 - (byte)word[i]); // 'a' + 'z'
+                else
+                    c = (char)(155 - (byte)word[i]); // 'A' + 'Z'
+
+                newWord += c;
+            }
+
+            return newWord;
+        }
     }
 }
